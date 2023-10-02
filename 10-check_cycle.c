ecd85c2beff840c64b54c39d8130706661031bca
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-02 16:54:50 -0400

    introductory to python interpreter, string, intergers and floats

---
+++ 10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+*check_cycle - check cycle in a linked list
+* @list:linked list
+* Return: 1-if cycle present 0-if not
+*/
+int check_cycle(listint_t *list)
+{
+	long int p = (long int)list;
+
+	if (list == NULL)
+		return (0);
+	list = list->next;
+	while (list != NULL)
+	{
+		if ((long int)list == p)
+			return (1);
+		list = list->next;
+	}
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+_october 2- 2023_
+PYTHON 3.4
+-INTRODUCTION TO PYTHON
+	pytho primitive data types
+		intergers
+		floats
+		strings
+	understanding python interpreter
+	pyhon statements
