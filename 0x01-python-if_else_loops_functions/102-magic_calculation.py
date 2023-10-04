16dabc569f5ebc0ee3b04befb77c988bd9a84d95
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-04 11:02:31 -0400

    generating a code function from dissasembled .py--correction

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -2,7 +2,7 @@
 def magic_calculation(a, b, c):
     if (a > b):
         if (c < b):
-            return (a + b) - c
+            return (a * b) - c
         else:
             return a + b
     else:
