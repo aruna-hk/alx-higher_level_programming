25d33203bd571fc30fe11c31969d158671c5d7ed
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-05 13:33:58 -0400

    unexecutable when not filename main

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,11 +2,7 @@
 from add_0 import add
 
 
-def imp():
+if __name__ == "__main__":
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
-
-
-if __name__ == "__main__":
-    imp()
