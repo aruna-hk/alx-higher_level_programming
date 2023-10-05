f4c5b1a3ed0d88462cf3ad5c52a7d9c05dd837dd
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-05 12:08:16 -0400

    displaying command line args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,14 @@ import sys
 
 
 def arguments():
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+    if (len(sys.argv) - 1 == 0):
+        print("{} arguments.".format(len(sys.argv) - 1))
+    elif (len(sys.argv) - 1 == 1):
+        print("{} argument:\n{}: {}".format(len(sys.argv) - 1, 1, sys.argv[1]))
+    else: 
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
 
 
 if __name__ == "__main__":
