4d7b17e6b1fd920161b76604f04a394209b4b5be
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-06 03:04:28 -0400

    update of simple calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,21 +2,23 @@
 import sys
 import calculator_1
 
-if (len(sys.argv) != 4):
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-if (len(sys.argv[2]) == 1 and sys.argv[2] == "+"):
-    n = calculator_1.add(int(sys.argv[1]), int(sys.argv[3]))
-    print("{} + {} = {}".format(sys.argv[1], sys.argv[3], n))
-elif (len(sys.argv[2]) == 1 and sys.argv[2] == "-"):
-    n = calculator_1.sub(int(sys.argv[1]), int(sys.argv[3]))
-    print("{} + {} = {}".format(sys.argv[1], sys.argv[3], n))
-elif (len(sys.argv[2]) == 1 and sys.argv[2] == "*"):
-    n = calculator_1.mul(int(sys.argv[1]), int(sys.argv[3]))
-    print("{} + {} = {}".format(sys.argv[1], sys.argv[3], n))
-elif (len(sys.argv[2]) == 1 and sys.argv[2] == "/"):
-    n = calculator_1.div(int(sys.argv[1]), int(sys.argv[3]))
-    print("{} + {} = {}".format(sys.argv[1], sys.argv[3], n))
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if (len(sys.argv[2]) == 1 and sys.argv[2] == "+"):
+        n = calculator_1.add(int(sys.argv[1]), int(sys.argv[3]))
+        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], n))
+    elif (len(sys.argv[2]) == 1 and sys.argv[2] == "-"):
+        n = calculator_1.sub(int(sys.argv[1]), int(sys.argv[3]))
+        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], n))
+    elif (len(sys.argv[2]) == 1 and sys.argv[2] == "*"):
+        n = calculator_1.mul(int(sys.argv[1]), int(sys.argv[3]))
+        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], n))
+    elif (len(sys.argv[2]) == 1 and sys.argv[2] == "/"):
+        n = calculator_1.div(int(sys.argv[1]), int(sys.argv[3]))
+        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], n))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
