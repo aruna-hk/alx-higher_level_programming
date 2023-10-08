c26520c25e07ed5939c5c47cdeb1c649e25779e5
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-08 15:13:46 -0400

    correction of function to display matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,10 +2,12 @@
 
 
 def print_matrix_integer(matrix=[[]]):
+    if (matrix is None):
+        return None
     for i in range(len(matrix)):
         j = 0
         for j in range(len(matrix[j])):
-            print("{}".format(matrix[i][j]), end='')
+            print("{:d}".format(matrix[i][j]), end='')
             if (j != (len(matrix[j]) - 1)):
                 print('', end=' ')
         print("")
