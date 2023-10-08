abc61f6d35343e7551276f7c49e91929e335de2e
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-08 14:05:53 -0400

    printing the content of a matrix in py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         j = 0
         for j in range(len(matrix[j])):
-            print("{} ".format(matrix[i][j]), end='')
+            print("{}".format(matrix[i][j]), end='')
+            if (j != (len(matrix[j]) - 1)):
+                print('', end=' ')
         print("")
