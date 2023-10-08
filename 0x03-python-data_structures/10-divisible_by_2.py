289e3d3079bd233095156c40eed9b91e731dcdf7
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-08 15:40:02 -0400

    check if values in a list are divisible by two and return a new list of true false values

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,5 +3,10 @@
 def divisible_by_2(my_list=[]):
     if (my_list is None):
         return None
-    list_l = [(i % 2 == 0) for i in range(len(my_list))]
+    list_l = []
+    for i in range(len(my_list)):
+        if (my_list[i] % 2 == 0):
+            list_l.append(True)
+        else:
+            list_l.append(False)
     return list_l
