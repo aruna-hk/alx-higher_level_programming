1e57ed3f430d4d37c06c2a59e60dad1c01aebc6d
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-12 03:01:31 -0400

    correction of change roma to decimal incase of non string arguement

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,6 +5,8 @@ def roman_to_int(roman_string):
     if (roman_string is None):
         return 0
     romc = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if (type(roman_string) is not str):
+        return 0
     roman_string.upper()
     num = 0
     prev = 0
