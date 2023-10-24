7281bb1bb2b100d8f7115000961988a70186cc7c
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-24 12:07:11 -0400

    instatiation of in class:

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -18,10 +18,8 @@ class Square:
             args:
                 size (int): instatiate private size attribute
         """
-        if isinstance(size, int):
-            if (size < 0):
-                raise(ValueError("size must be >= 0"))
-            else:
-                self.__size = size
-        else:
+        if (type(size) is not int):
             raise(TypeError("size must be an integer"))
+        if (size < 0):
+            raise(ValueError("size must be >= 0"))
+        self.__size = size
