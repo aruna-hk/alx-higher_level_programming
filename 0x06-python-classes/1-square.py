52089fa3f1b8a7c09aaef9dc6591cac5b9723795
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-24 11:35:29 -0400

    instatiation of private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+"""
+    class square creates an object with private attriute size
+"""
+
+
+class Square:
+    """ square class
+        does nothing
+
+        Args:
+            size: no type
+        Attrubutes:
+            size: no type
+    """
+    def __init__(self, size):
+        self.__size = size
