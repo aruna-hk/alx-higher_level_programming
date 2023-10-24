b756e1d8baf969d1ad1ee9be055da760e42b98f3
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-24 11:47:43 -0400

    instatiating specific da type

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,15 +7,17 @@
 
 
 class Square:
-    """ square class
-        does nothing
+    """ square class does nothing
 
-        Args:
-            size (int): to instatiate private size attribute
         Attrubutes:
             size (int): to instatiate private size attribute
     """
     def __init__(self, size=0):
+        """ init function -instaiates orcts attributes
+
+            args:
+                size (int): instatiate private size attribute
+        """
         if isinstance(size, int):
             if (size < 0):
                 raise(ValueError("size must be >= 0"))
