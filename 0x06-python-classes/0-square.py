65e15bad5c4d5a53d118c0e32eac2acd6cf10b47
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-24 11:20:23 -0400

    python oop concepts

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    """ square class
+
+        does nothing
+        Args: None
+        Attrubutes: None
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,5 @@
+24 th october 2023
+
+object orinted programming with python
+
+<author><kiptooharon.hk@gmail.com>
