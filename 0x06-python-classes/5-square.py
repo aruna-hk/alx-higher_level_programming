136e12e2442924dd293cc0f24093e763c15f2644
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-26 12:37:18 -0400

    pycode documentation and err_msg typo

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -12,16 +12,16 @@ class Square:
         Attrubutes:
             size (int): to instatiate private size attribute
     """
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """ init function -instaiates orcts attributes
 
             args:
                 size (int): instatiate private size attribute
         """
         if (type(size) is not int):
-            raise(TypeError("size must be an integer"))
+            raise (TypeError("size must be an integer"))
         if (size < 0):
-            raise(ValueError("size must be >= 0"))
+            raise (ValueError("size must be >= 0"))
         self.__size = size
 
     def area(self):
@@ -40,9 +40,9 @@ class Square:
                 value: value to set size to
         """
         if (type(value) is not int):
-            raise(TypeError("size must be integer"))
+            raise (TypeError("size must be an integer"))
         if (value < 0):
-            raise(ValueError("size must be >= 0"))
+            raise (ValueError("size must be >= 0"))
         self.__size = value
 
     def my_print(self):
@@ -53,3 +53,14 @@ class Square:
             for i in range(self.__size):
                 print("#", end='')
             print("")
+
+    @property
+    def position(self):
+        """ position getter """
+        return (self_.position)
+
+    @position.setter
+    def position(self, value):
+        if (isinstance(value, tuple) is not True):
+            raise (TypeError("position must be a tuple of 2 positive integers"))
+        self.__position = value
