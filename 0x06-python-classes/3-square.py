9b3a508be6192412dfdedf1edec55f10a494f4cb
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-26 12:32:48 -0400

    pycodestyle documentation

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -19,15 +19,15 @@ class Square:
                 size (int): instatiate private size attribute
         """
         if (type(size) is not int):
-            raise(TypeError("size must be an integer"))
+            raise (TypeError("size must be an integer"))
         if (size < 0):
-            raise(ValueError("size must be >= 0"))
+            raise (ValueError("size must be >= 0"))
         self.__size = size
 
     def area(self):
         """ area - return square of private self """
         if (type(self.__size) is not int):
-            raise(TypeError("size must be an integer"))
+            raise (TypeError("size must be an integer"))
         if (self.__size < 0):
-            raise(ValueError("size must be >= 0"))
+            raise (ValueError("size must be >= 0"))
         return self.__size ** 2
