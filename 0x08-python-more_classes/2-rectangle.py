fbd86ac7d5c056d0afc39f9defda206552720cc2
kiptoo <kiptoharon.hk@gmail.com>
2023-10-30 14:52:34 -0400

    extension of rectangle class introduction of area and perimeter methods

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,55 +1,53 @@
 #!/usr/bin/python3
 
-""" class module
-
-    perform operation on retangle
-    functions:
-        width:getter and setter functions
-        area: return area of the perimeter
-        perimeter: return perimeter of the rectangle
-
-"""
+""" class module"""
 
 
 class Rectangle:
+    """ class rectangle define and recangle functions"""
 
     def __init__(self, width=0, height=0):
         """init method"""
-        self.__width = width
-        self.__height = height
+
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """width getter"""
+
         return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+
+        if (type(value) is not int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
         """height getter"""
+
         return (self.__height)
 
     @height.setter
     def height(self, value):
         """height setter"""
+
         if ((isinstance(value, int)) is False):
             raise (TypeError("height must be an integer"))
         if (value < 0):
             raise (ValueError("height must be >= 0"))
         self.__height = value
 
-    @width.setter
-    def width(self, value):
-        """width setter"""
-        if (isinstance(value, int) is False):
-            raise (TypeError("width must be an integer"))
-        if (value < 0):
-            raise (ValueError("width must be >= 0"))
-        self.__width = value
-
     def area(self):
-        """ compute are of rectangle """
-        return ((self.__width) * (self.__height))
+        """compute area of rectangle"""
+        return (self.__width * self.__height)
 
     def perimeter(self):
-        """ compute perimeter of rectngle"""
+        """compute perimeter of rectangle"""
         return ((2 * self.__width) + (2 * self.__height))
