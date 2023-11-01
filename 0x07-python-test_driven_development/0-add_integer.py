a9b2f83434dc19026c75b389613401fdb53199fb
kiptoo <kiptooharon.hk@gmail.com>
2023-11-01 13:08:27 +0300

    add integer correction

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,4 +17,4 @@ def add_integer(a, b=98):
         raise (TypeError("a must be an integer"))
     if ((isinstance(b, int) is False) and (isinstance(b, float) is False)):
         raise (TypeError("b must be an integer"))
-    return (int(a) + int(b))
+    return int(a + b)
