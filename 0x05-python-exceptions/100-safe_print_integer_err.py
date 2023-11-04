9f31e58905fe1af0cefa3fbcb6e734f9346f0e2b
kiptoo <kiptooharon.hk@gmail.com>
2023-11-04 07:08:34 -0400

    safe print integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception:
-        sys.stderr.write("{}".format(Exception))
+        sys.stderr.write("Exception:{}".format(Exception))
         return False
