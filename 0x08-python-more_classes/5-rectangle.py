304dce2b619234b51a0937d75429057e56554114
kiptoo <kiptooharon.hk@gmail.com>
2023-11-05 13:56:12 -0500

    implementing special method __del__ on user defined class

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+
+""" class module"""
+
+
+class Rectangle:
+    """ class rectangle define and recangle functions"""
+
+    def __init__(self, width=0, height=0):
+        """init method"""
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """width getter"""
+
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+
+        if (type(value) is not int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+
+        if ((isinstance(value, int)) is False):
+            raise (TypeError("height must be an integer"))
+        if (value < 0):
+            raise (ValueError("height must be >= 0"))
+        self.__height = value
+
+    def area(self):
+
+        """compute area of rectangle"""
+        return (self.height * self.width)
+
+    def perimeter(self):
+        """compute perimeter of rectangle"""
+
+        if ((self.width == 0) or (self.height == 0)):
+            return 0
+        return (2 * (self.width + self.height))
+
+    def __str__(self):
+        """String representation of the object"""
+
+        lis = []
+        if ((self.width == 0) or (self.height == 0)):
+            st = ''.join(str(i) for i in lis)
+            return st
+        return '\n'.join(['#' * self.__width] * self.__height)
+
+    def __repr__(self):
+        """actual object representation """
+
+        o = "{}".format(self.__class__.__name__)
+        p = "({}, {})".format(self.__width, self.__height)
+        return o + p
+
+    def __del__(self):
+        """delete object"""
+
+        print("Bye rectangle...")
