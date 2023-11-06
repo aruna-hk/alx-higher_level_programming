2c04a82fbe0c4b39e0bf337f0ce8a09d75f13b18
kiptoo <kiptooharon.hk@gmail.com>
2023-11-07 02:13:54 +0300

    overiding  __str__ from super class

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" square module """
+
+
+class Square(__import__('9-rectangle').Rectangle):
+    """class square """
+
+    def __init__(self, size):
+        """ init method"""
+
+        self.integer_validator(None, size)
+        self.__size = size
+        __import__('9-rectangle').Rectangle.__init__(self, size, size)
+
+    def area(self):
+        """ perform area of square """
+
+        return self.__size ** 2
+
+    def __str__(self):
+        """ string representation"""
+
+        return f"[Square] {self.__size}/{self.__size}"
