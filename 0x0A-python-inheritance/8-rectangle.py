e301c65c31380a4fcc045cbbe896ad4fb310f77f
kiptoo <kiptooharon.hk@gmail.com>
2023-11-07 01:35:13 +0300

    update of integer validator

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,7 +6,6 @@ class Rectangle(__import__('7-base_geometry').BaseGeometry):
     """ class rectangle inherits from bas geometry """
 
     def __init__(self, width, height):
-        self.integer_validator(self.__name__, width);
-        self.integer_validator(self.__name__, height);
+        __import__('7-base_geometry').BaseGeometry.__init__
         self.__width = width
         self.__height
