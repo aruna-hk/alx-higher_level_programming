11a1962429f8b3024a4944b71fc3bfccef466988
kiptoo <kiptooharon.hk@gmail.com>
2023-11-07 22:44:38 +0300

    writing to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ def write_file(filename="", text=""):
     """ open/create file for writing"""
 
     with open(filename, "w", encoding='utf-8') as file:
-        file.write(text)
+        return(file.write(text))
