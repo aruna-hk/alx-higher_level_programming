ba96435648def02125a2dbddbc523b18126b21de
kiptoo <kiptooharon.hk@gmail.com>
2023-11-08 13:17:27 -0500

    relading jason file to python object

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+""" class student  json representation """
+import json
+
+
+class Student:
+    """ class student attributes: firstname, lastname, to_json"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ return jason representation of class instance"""
+
+        if attrs is None:
+            return self.__dict__
+        return {k: v for k, v in self.__dict__.items() if k in attrs}
+
+    def reload_from_json(self, json):
+        """ reload obect attributes from json file """
+
+        self.__dict__ = json
