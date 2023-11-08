f2b87c4f145260519759cb37469860ffc7b39d3f
kiptoo <kiptooharon.hk@gmail.com>
2023-11-08 10:23:43 -0500

    serializing objects dictionary

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" take object and return its dictionary to e serialized y json"""
 import json
 
 
 def class_to_json(obj):
-    return json.dumps(obj.__dict__)
+    """ return object dictioary to e serialized"""
+
+    return obj.__dict__
