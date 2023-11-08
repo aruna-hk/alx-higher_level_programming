8b8b4858b38db703d32155cf999f1bd751650349
kiptoo <kiptooharon.hk@gmail.com>
2023-11-08 08:37:19 -0500

    converting python class instance to json object

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-""" representing python class objct as json object """
 import json
 
 
 def class_to_json(obj):
-    """ dumps python class object to json object """
-
     return json.dumps(obj.__dict__)
