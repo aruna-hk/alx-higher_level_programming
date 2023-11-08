22a3eaa6b4e9d52772d95b67d93e4ace30a34912
kiptoo <kiptooharon.hk@gmail.com>
2023-11-08 11:30:49 -0500

    added function to return dictionary reprsentation of instance

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -16,4 +16,4 @@ class Student:
 
         if attrs is None:
             return self.__dict__
-        return {key:value for key, value in self.__dict__.items() if key in attrs}
+        return {k: v for k, v in self.__dict__.items() if k in attrs}
