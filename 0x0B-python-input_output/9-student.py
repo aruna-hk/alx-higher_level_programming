e3bc0c70b285b9ed140189b753bab67cd43683ea
kiptoo <kiptooharon.hk@gmail.com>
2023-11-08 10:57:36 -0500

    adding class function to return serializable __dict__ objcts

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,12 +4,11 @@ import json
 
 
 class Student:
-    """ class student
-    attributes:
-    firstname, lastname, to_jso
-    """
+    """ class student attr firstname, lastname, age, to_json"""
 
     def __init__(self, first_name, last_name, age):
+        """ return jason representation of class instance"""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -17,4 +16,5 @@ class Student:
     def to_json(self):
         """ return jason representation of class instance"""
 
-        return self.__dict__
+        serializable = __import__('8-class_to_json').class_to_json
+        return serializable(self)
