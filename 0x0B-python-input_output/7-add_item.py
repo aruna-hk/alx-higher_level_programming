097e43c50321ac1eb3f6343db70067e0e91a93c8
kiptoo <kiptooharon.hk@gmail.com>
2023-11-08 08:11:30 -0500

    loading sys.args to jason file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ this module bundle all arguments to a list except file name"""
+
+
 import sys
 save_json = __import__('5-save_to_json_file').save_to_json_file
 return_json = __import__('6-load_from_json_file').load_from_json_file
