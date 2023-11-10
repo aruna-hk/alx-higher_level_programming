8a3a3505b7c4c2e39bca6c9f379602858b29070b
kiptoo <kiptooharon.hk@gmail.com>
2023-11-10 08:42:52 -0500

    function to calculate weighted mean

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if my_list is None:
+    if ((my_list is None) or (len(my_list) == 0)):
         return 0
     total = 0
-    t_weigh = 0
+    num = 0
     for i in range(len(my_list)):
-        total = total + (my_list[i][0] * my_list[i][1])
-        t_weigh = t_weigh + my_list[i][1]
-    return total / t_weigh
+        total += my_list[i][1] * my_list[i][0]
+        num += my_list[i][1]
+    return total/num
