ef406ca1596519b84b342dc5d651dd7d3d776c23
kiptoo <kiptooharon.hk@gmail.com>
2023-11-10 05:27:03 -0500

    correction of simple_calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
         print("{} + {} = {}".format(sys.argv[1], sys.argv[3], n))
     elif (len(sys.argv[2]) == 1 and sys.argv[2] == "-"):
         n = calculator_1.sub(int(sys.argv[1]), int(sys.argv[3]))
-        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], n))
+        print("{} - {} = {}".format(sys.argv[1], sys.argv[3], n))
     elif (len(sys.argv[2]) == 1 and sys.argv[2] == "*"):
         n = calculator_1.mul(int(sys.argv[1]), int(sys.argv[3]))
-        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], n))
+        print("{} * {} = {}".format(sys.argv[1], sys.argv[3], n))
     elif (len(sys.argv[2]) == 1 and sys.argv[2] == "/"):
         n = calculator_1.div(int(sys.argv[1]), int(sys.argv[3]))
-        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], n))
+        print("{} / {} = {}".format(sys.argv[1], sys.argv[3], n))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
