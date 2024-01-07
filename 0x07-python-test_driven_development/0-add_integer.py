2291e24ec6c9ab7e623a197f020c66266d9a62af
kiptoo haron <kiptooharon.hk@gmail.com>
2024-01-08 01:24:55 +0300

    add integer function and test cases update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,8 +14,8 @@ def add_integer(a, b=98):
         and return int of a and b"""
 
     err_str = "{} must be an integer"
-    if type(a) is chr or type(a) is str:
+    if type(a) is chr or type(a) is str or a is None:
         raise TypeError(err_str.format("a"))
-    if type(b) is chr or type(b) is str:
+    if type(b) is chr or type(b) is str or b is None:
         raise TypeError(err_str.format("b"))
     return int(a) + int(b)
