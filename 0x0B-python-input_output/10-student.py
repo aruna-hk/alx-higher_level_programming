c6ecb6cff69d42fc8752a5848dd84abb4420098c
kiptoo haron <kiptooharon.hk@gmail.com>
2024-01-10 16:31:28 +0300

    serializing obj attributes

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-
-""" class student  json representation """
+""" this module contains class student and to-json function
+    that return dict representation of obj
+"""
 
 
 class Student:
-    """ class student attributes: firstname, lastname, to_json"""
+    """ class student attr firstname, lastname, age, to_json"""
 
     def __init__(self, first_name, last_name, age):
+        """ return jason representation of class instance"""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ return jason representation of class instance"""
+        """ return oject attributes for json srialization"""
 
-        if attrs is None:
+        if attrs is None or type(attrs) is not list:
             return self.__dict__
-        return {k: v for k, v in self.__dict__.items() if k in attrs}
+        else:
+            new_dict = {}
+            for key in attrs:
+                if key in self.__dict__.keys():
+                    new_dict[key] = self.__dict__[key]
+            return new_dict
