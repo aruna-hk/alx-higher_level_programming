dde1b3509c1f72c48b66261f363692058ba95d9e
kiptoo haron <kiptooharon.hk@gmail.com>
2024-01-10 15:51:46 +0300

    add std in argument to a list and save to file script

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-""" this module bundle all arguments to a list except file name"""
+""" this module bundle all arguments to a list and save on file"""
 
 
 import sys
+import json
 save_json = __import__('5-save_to_json_file').save_to_json_file
 return_json = __import__('6-load_from_json_file').load_from_json_file
 
-
-lis = sys.argv[1:]
-save_json(lis, "add_item.json")
-return_json("add_item.json")
+try:
+    pyobj = return_json("add_item.json")
+    save_json(pyobj + sys.argv[1:], "add_item.json")
+except FileNotFoundError:
+    if (sys.argv is not None):
+        save_json(sys.argv[1:], "add_item.json")
