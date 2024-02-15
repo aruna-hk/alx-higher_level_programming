20d2d694c0825e085eec45112d58326319254676
kiptoo haron <kiptooharon.hk@gmail.com>
2024-02-15 14:13:09 +0300

    creat table and insert values

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- creat table and insert values
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table
+VAlUES
+	(1, "John", 0),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
