41ef27b33b5a07970f1f7d3a4f91b78e37531646
kiptoo haron <kiptooharon.hk@gmail.com>
2024-03-13 21:03:49 +0300

    checkig command line arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 //command line arguments
-if ( ( process.argv.length ) > 2 ) {
-  console.log('Arguments found')
+let arglen = process.argv.length;
+if (arglen <= 2) {
+  console.log('No arguments');
+}
+else if (arglen === 3){
+  console.log('Argument found');
 }
 else {
-  console.log('No arguments')
+  console.log('Arguments found');
 }
