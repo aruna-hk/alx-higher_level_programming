369db05a7e098bc26d581db379be65ce6e5f7696
kiptoo haron <kiptooharon.hk@gmail.com>
2024-03-13 20:46:08 +0300

    string concat with js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-/*
-* multiline string variable
-*/
-const myVar = "C is fun\nPython is cool\nJavaScript is amazing"
-console.log(myVar)
+// multiline string variable
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
