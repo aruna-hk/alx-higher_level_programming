cc41da7d1bae69fea68dfe21b76d8644ecfd7ed2
kiptoo haron <kiptooharon.hk@gmail.com>
2024-03-13 23:42:51 +0300

    function to reexecute function a number of times

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,5 @@
-#!/usr/bin/python3
-/**
- * execute function several times
- */
+#!/usr/bin/node
+//call and execute function some numeber of times
 exports.callMeMoby = function(x, thefunction) {
   let i = 0;
   while (i < x) {
