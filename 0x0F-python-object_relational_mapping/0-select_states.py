a943b7ba1defef29c7b671fb0361215b17c4e667
hk <kiptooharon.hk@gmail.com>
2024-03-17 09:31:14 -0400

    database connectio with mysqldb and databas querying

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" this module contains script for connection to mysql server using
+    mysqldb connector, and using the connection to query database
+    ./thisscript mysql username, mysql password and database name
+"""
+
+import MySQLdb
+from sys import argv
+
+connectioninfo = {
+    "port": 3306,
+    "user": argv[1],
+    "password": argv[2],
+    "db": argv[3]
+}
+query = "select * from states"
+connection = MySQLdb.connect(**connectioninfo)
+connection.query(query)
+result = connection.store_result()
+selected = result.fetch_row(maxrows=0)
+for row in selected:
+    print(row)
+connection.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,5 @@
+python database conectivity
+  - mysql database conection with mysqldb
+  - package MySQLdb
+  - pip install mysql
+python object relatio mapping with sql alchemy
