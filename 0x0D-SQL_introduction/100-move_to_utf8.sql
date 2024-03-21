22a6ac5dbaf1bf6712ca35806f9f026d3f09580e
hk <kiptooharon.hk@gmail.com>
2024-03-21 12:11:01 -0400

    chrcter encoding change

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,13 @@
+-- convert database encoding
+USE mysql;
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+USE hbtn_0c_0;
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table
+MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
