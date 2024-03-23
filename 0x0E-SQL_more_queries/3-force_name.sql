330c608adeac793ea516439f8bca791d2162c761
hk <kiptooharon.hk@gmail.com>
2024-03-23 08:36:13 -0400

    creatind daatabase tables, database and users

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,2 @@
+-- create table in databas passed as argument
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- table wit default values
+CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
