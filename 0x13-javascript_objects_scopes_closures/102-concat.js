6143653f12b8125ffb0bd474a4a0ecd8e624edb5
hk <kiptooharon.hk@gmail.com>
2024-03-29 11:19:16 +0300

    js script to concat file content

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
 const f = require('fs');
-const f1_c = f.readFileSync(process.argv[2]).toString();
-const f2_c = f.readFileSync(process.argv[3]).toString();
-f.writeFileSync(process.argv[4], f1_c + f2_c);
+const first_arg = f.readFileSync(process.argv[2]).toString();
+const scond_arg = f.readFileSync(process.argv[3]).toString();
+f.writeFileSync(process.argv[4], first_arg + second_arg);
