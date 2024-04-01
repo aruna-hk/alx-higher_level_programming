96fce205ef5ac4922abd2705ec052bb984f7afce
hk <kiptooharon.hk@gmail.com>
2024-04-01 23:51:24 +0300

    get message body len from header

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 #cURL body size
 #get the body size of the response
 
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
+curl -sI "$1" | grep -i "Content-Length" | awk -F ': ' '{print $2}'
