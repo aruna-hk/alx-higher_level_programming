63878deed3190fd44e9ee5eae1b1497de4d0907a
hk <kiptooharon.hk@gmail.com>
2024-05-24 05:31:27 +0300

    counting number of appearances of actor in episodes

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
 
-const req = require('request');
+const request = require('request');
 const urlc = 'https://swapi-api.alx-tools.com/api/people/18/';
-
-req(process.argv[2], (error, response, body) => {
+request(process.argv[2], (error, response, body) => {
   if (error) {
-    console.log(error);
     return;
   }
   let count = 0;
-  const data = JSON.parse(body);
-  const movies = data.results;
+  const movies = JSON.parse(body).results;
   for (let i = 0; i < movies.length; i++) {
     if (movies[i].characters.includes(urlc)) {
       count = count + 1;
