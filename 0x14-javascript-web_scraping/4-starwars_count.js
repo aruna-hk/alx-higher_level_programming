0339cbe86bae63298cf67f3d566a20a38747883a
hk <kiptooharon.hk@gmail.com>
2024-05-24 08:52:23 +0300

    number of movies where the character “<char-id>” is present.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,30 @@
 #!/usr/bin/node
 
 const request = require('request');
-const urlc = 'https://swapi-api.alx-tools.com/api/people/18/';
-request(process.argv[2], (error, response, body) => {
-  if (error) {
-    return;
-  }
+
+async function fetchMovies (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+}
+
+async function main () {
+  const urlc = 'https://swapi-api.alx-tools.com/api/people/18/';
   let count = 0;
-  const movies = JSON.parse(body).results;
+  const response = await fetchMovies(process.argv[2]);
+  const movies = JSON.parse(response).results;
   for (let i = 0; i < movies.length; i++) {
     if (movies[i].characters.includes(urlc)) {
       count = count + 1;
     }
   }
   console.log(count);
-});
+}
+
+main();
