3cbb645dc2c6cba5f94ede62e6a7573bcf355603
hk <kiptooharon.hk@gmail.com>
2024-05-24 03:32:22 +0300

    Revert "sychronous js readfile"
    This reverts commit 97e29365cbd296a084b6c369fefe74554fc5988e.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,5 +4,9 @@
 */
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
-    console.error(err || data);
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
 });
