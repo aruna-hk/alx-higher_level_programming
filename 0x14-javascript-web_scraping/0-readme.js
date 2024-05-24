3c03154e3389a7772d1dba590c8461fd2e8498b6
hk <kiptooharon.hk@gmail.com>
2024-05-24 03:18:34 +0300

    read file in node

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,8 +5,8 @@
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
     if (err) {
-        console.error('Error reading file:', err);
+        console.error(err);
         return;
     }
-    console.log('File content:', data);
+    console.log(data);
 });
