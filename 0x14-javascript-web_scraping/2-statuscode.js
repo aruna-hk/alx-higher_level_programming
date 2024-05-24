42d70d05f26a115d9d5c8a57a954867d6c03e5c0
hk <kiptooharon.hk@gmail.com>
2024-05-24 04:02:05 +0300

    inspecting status code:

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
+/*
+ * read response status code
+ */
 const request = require("request");
 request(process.argv[2], (error, response, body) => {
-    console.log("code: ",response.statusCode);
+    console.log("code:",response.statusCode);
 });
