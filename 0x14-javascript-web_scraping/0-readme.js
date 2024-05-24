97e29365cbd296a084b6c369fefe74554fc5988e
hk <kiptooharon.hk@gmail.com>
2024-05-24 03:31:04 +0300

    sychronous js readfile

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,5 @@
 */
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+    console.error(err || data);
 });
