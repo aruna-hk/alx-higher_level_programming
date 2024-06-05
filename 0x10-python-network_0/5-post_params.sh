33c47cd902b93862eee002bb76f55214880de55b
hk <kiptooharon.hk@gmail.com>
2024-06-05 13:52:22 +0300

    sending post rquest with data

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #post json data
-curl "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl "$1" -X POST -d "email=hr%37holbertonschool%46com&subject=I%32will%32always%32be%32here%32for%32PLD"
