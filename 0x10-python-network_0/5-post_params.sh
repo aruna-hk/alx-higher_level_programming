6975b37b0cf5640b2460e6e0bc94b30beb99651f
hk <kiptooharon.hk@gmail.com>
2024-06-05 13:49:30 +0300

    sending json data with post request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #post json data
-curl "$1" -sLX POST -H "'Content-Type':'application/json'" -d "{'email':'hr@holbertonschool.com','subject':'I will always be here for PLD'}"
+curl "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
